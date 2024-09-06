e91ff68fc83956e99cead8180d399e52ab29d0cb
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-09-06 09:22:52 +0500

    js

--- src/components/worcks/index.jsx
+++ src/components/worcks/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import './style.scss';
-import icon from '../../assets/icon.svg';
+import comp from '../../assets/comp.svg';
 
 const steps = [
   {
     title: 'Setup Your wallet',
     description: 'Set up your wallet of choice. Connect it to the NFT market by clicking the wallet icon in the top right corner.',
-    image: icon,
+    image: comp,
   },
   {
     title: 'Create Collection',
     description: 'Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.',
-    image: icon,
+    image: copm,
   },
   {
     title: 'Start Earning',
     description: 'Upload your work and setup your collection. Add a description, social links, and floor price.',
-    image: icon,
+    image: comp,
   },
 ];
 
@@ -29,7 +29,7 @@ const Index = () => {
         {steps.map((step, index) => (
           <div className="works-box-itms" key={index}>
             <div className="works-img">
-              <img src={icon} alt={step.title} />
+              <img src={comp} alt={step.title} />
             </div>
             <div className="works-text">
               <p className="works-text-title">{step.title}</p>
